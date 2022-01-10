5ce4208c5ea6b20336336fc914ede2e8764cb3b1
coool589 <coool589@naver.com>
2022-01-11 01:56:49 +0900

    added I/F frontend to backend

--- frontend/src/service/language/FilmRate.js
+++ frontend/src/service/language/FilmRate.js
@@ -3,18 +3,48 @@ import { Component } from 'react';
 
 class FilmRate extends Component {
     state = {
-        review: '',
+        review: "이 영화 진짜 존잼이다 하하하하핳",
         rate: 0.0,
+        progress: 0,
+    }
+    constructor(props) {
+        super(props);
+        //this.handleChange = this.handleChange.bind(this);
+        this.evaluateComment = this.evaluateComment.bind(this);
     }
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
+    evaluateComment() {
+        fetch("http://127.0.0.1:8000/filmrate/predict", {
+          method: "POST",
+          headers: {
+            'Content-type': 'application/json'
+        },
+          body: JSON.stringify({
+            content: this.state.review
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data !== null && data["rate"] !== null) {
+            let rate = data["rate"];
+            let ratio = rate * 100 / 5;
+            console.log(ratio);
+            
+            this.setState({
+              rate: data["rate"],
+              progress: ratio
+            });
+          }
+        });
+    }
     render() {
         return (
             <div class="component">
-                <form>
+                {/* <form> */}
                     <div class="section">
                         <input
                             class="content"
@@ -24,14 +54,17 @@ class FilmRate extends Component {
                         />
                         {/* <div>{this.state.review}</div> */}
                         <div>
-                            <button>Evaluate</button>
+                            <button onClick={this.evaluateComment}>Evaluate</button>
                         </div>
                     </div>
 
                     <div class="section">
                         <div>Expected Rate : {this.state.rate}</div>
+                        <div className="progress-div">
+                          <div style={{width: `${this.state.progress}%`}} className="progress"/>
+                        </div>
                     </div>
-                </form>
+                {/* </form> */}
             </div>
         );
     }
