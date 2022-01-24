989f276710f126bf21e63be6ff06c16b3ffc18a3
coool589 <coool589@naver.com>
2022-01-24 09:01:37 +0900

    modify frontend filmrate to request one-by-one (buttons are nested in table)

--- frontend/src/service/language/FilmRate.js
+++ frontend/src/service/language/FilmRate.js
@@ -9,118 +9,128 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 class FilmRate extends Component {
-    state = {
-        review: "이 영화 진짜 존잼이다 하하하하핳",
-        rate: 0.0,
-        progress: 0,
-    }
+    // static MovieRate = {
+    //     modelName: PropTypes.string.isRequired,
+    //     rate: PropTypes.number.isRequired,
+    // };
+
     constructor(props) {
         super(props);
-        //this.handleChange = this.handleChange.bind(this);
         this.evaluateComment = this.evaluateComment.bind(this);
+
+        this.state = {
+          review: "이 영화 진짜 존잼이다 하하하하핳",
+          movieRates: [
+              {
+                  modelId: 1,
+                  modelName: "Model 01",
+                  rate: 0.0,
+              },
+              {
+                  modelId: 2,
+                  modelName: "Model 02",
+                  rate: 0.0,
+              },
+              {
+                  modelId: 3,
+                  modelName: "Model 03",
+                  rate: 0.0,
+              }
+          ]
+        };
     }
     handleChange = (e) => {
-      //console.log(e.target.value);
         this.setState({
             [e.target.name]: e.target.value
         })
     }
-    evaluateComment() {
+    evaluateComment(modelId) {
         fetch("http://127.0.0.1:8000/filmrate/predict", {
           method: "POST",
           headers: {
             'Content-type': 'application/json'
         },
           body: JSON.stringify({
+            modelId: modelId,
             content: this.state.review
           })
         })
         .then(response => response.json())
-        .then(data => {
-          if (data !== null && data["rate"] !== null) {
-            let rate = data["rate"];
-            let ratio = rate * 100 / 5;
-            console.log(ratio);
-            
-            this.setState({
-              rate: data["rate"],
-              progress: ratio
-            });
-          }
-        });
+        .then(jsonRes => jsonRes["result"])
+        .then(result => {
+            if (result != null) {
+                const modelId = result["modelId"];
+                const rate = result["rate"];
+
+                let _movieRates = [...this.state.movieRates];
+                _movieRates.forEach((movieRate) => {
+                    if (movieRate.modelId === modelId) {
+                        movieRate.rate = rate;
+                    }
+                });
+
+                this.setState({
+                    movieRates: _movieRates
+                })
+            }
+        })
     }
 
     render() {
         return (
             <div class="component">
-                {/* <form> */}
-                    <div class="section">
-                      {/* <textarea name="review" class="content" value={this.state.review} onChange={this.handleChange}></textarea> */}
-                      {/* <input
-                          class="content"
-                          value={this.state.review}
-                          onChange={this.handleChange}
-                          name="review"
-                      /> */}
-                      {/* <div>{this.state.review}</div> */}
-                      <TextField multiline size="small" label="Review" fontWeight="{500}"
-                        sx={{ minWidth:700, maxWidth: 750, minHeight:10 }}
-                        inputProps={{style: {fontFamily: ['Malgun Gothic'], fontSize: 13, heigth: 22}}}
-                        name="review" value={this.state.review} onChange={this.handleChange}>
-                      </TextField>
-                      <div class="inline">
+                <div class="section">
+                    <TextField multiline size="small" label="Review" fontWeight="{500}"
+                    sx={{ minWidth:700, maxWidth: 750, minHeight:10 }}
+                    inputProps={{style: {fontFamily: ['Malgun Gothic'], fontSize: 13, heigth: 22}}}
+                    name="review" value={this.state.review} onChange={this.handleChange}>
+                    </TextField>
+                    {/* <div class="inline">
                         <Button variant="contained" endIcon={<SendIcon />} 
-                          size="middle"
-                          onClick={this.evaluateComment}>Eval</Button>
-                      </div>
-                    </div>
-
-                    {/* <div class="section">
-                        <div>Expected Rate : {this.state.rate}</div>
-                        <div className="progress-div">
-                          <div style={{width: `${this.state.progress}%`}} className="progress"/>
-                        </div>
-                        <Rating value={this.state.rate} precision={0.5} size={'large'} readOnly></Rating>
+                            size="middle"
+                            onClick={this.evaluateComment}>Eval</Button>
                     </div> */}
-                    <div class="section">
-                    <TableContainer component={Paper} sx={{ minWidth: 350, maxWidth:700 }}>
-                      <Table aria-label="simple table">
-                        <TableHead>
-                          <TableRow>
-                            <TableCell>Model</TableCell>
-                            <TableCell>Expected Rate</TableCell>
-                            {/* <TableCell align="right">Calories</TableCell> */}
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          <TableRow>
-                            <TableCell>Basic</TableCell>
-                            <TableCell>
-                              <Rating value={this.state.rate} precision={0.5} size={'large'} readOnly></Rating> ({this.state.rate})
-                            </TableCell>
-                          </TableRow>
-                          {/* {rows.map((row) => (
-                            <TableRow
-                              key={row.name}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                              <TableCell component="th" scope="row">
-                                {row.name}
-                              </TableCell>
-                              <TableCell align="right">{row.calories}</TableCell>
-                              <TableCell align="right">{row.fat}</TableCell>
-                              <TableCell align="right">{row.carbs}</TableCell>
-                              <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                          ))} */}
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
+                </div>
+
+                {/* <div class="section">
+                    <div>Expected Rate : {this.state.rate}</div>
+                    <div className="progress-div">
+                        <div style={{width: `${this.state.progress}%`}} className="progress"/>
                     </div>
-                {/* </form> */}
+                    <Rating value={this.state.rate} precision={0.5} size={'large'} readOnly></Rating>
+                </div> */}
+                <div class="section">
+                <TableContainer component={Paper} sx={{ minWidth: 350, maxWidth:700 }}>
+                    <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                        <TableCell>Model</TableCell>
+                        <TableCell>Expected Rate</TableCell>
+                        <TableCell>Evaluate</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.movieRates.map(({modelId, modelName, rate}) => (
+                            <TableRow key={modelId}>
+                                <TableCell>{modelName}</TableCell>
+                                <TableCell>
+                                    <Rating value={rate} precision={0.5} size={'large'} readOnly></Rating> ({rate})
+                                </TableCell>
+                                <TableCell>
+                                    <Button id={modelId} variant="contained" endIcon={<SendIcon />} size="middle" onClick={() => this.evaluateComment(modelId)}>
+                                        Start
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    </Table>
+                </TableContainer>
+                </div>
             </div>
         );
     }
